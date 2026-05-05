25e4f657dec52354c7af395e7eb0aa81e4e3ca95
oviquezTECSC <149113781+oviquezTECSC@users.noreply.github.com>
2026-05-05 08:04:13 -0600

    latest version type checker

--- src/typechecker/AlphaCompilerTypeChecker.java
+++ src/typechecker/AlphaCompilerTypeChecker.java
@@ -2,6 +2,7 @@ package typechecker;
 
 import org.antlr.v4.runtime.Token;
 import syntaxchecker.generated.AlphaCompilerBaseVisitor;
+import syntaxchecker.generated.AlphaCompilerLexer;
 import syntaxchecker.generated.AlphaCompilerParser;
 
 import java.util.LinkedList;
@@ -35,6 +36,9 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
         );
     }
 
+    //todo: otro reportError pero que reciba también dos tipos de datos (int)
+    //todo: aquellas llamas donde se requiera más info (tipos), cambiarlas por este nuevo reportError
+
     @Override
     public Object visitProgram(AlphaCompilerParser.ProgramContext ctx) {
         return super.visitProgram(ctx);
@@ -47,40 +51,50 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
 
     @Override
     public Object visitAssignSingleCommand(AlphaCompilerParser.AssignSingleCommandContext ctx) {
-        int exprType = (int) visit(ctx.expression());
-        TablaSimbolos.Ident ident = this.symbolTable.buscar(ctx.ID().getText());
-        if (ident != null){
-            if (!(ident instanceof TablaSimbolos.MethodIdent)){
-                if (((TablaSimbolos.VarIdent)ident).isConstant == false){
-                    if (ident.type!=exprType)
-                        reportError("Incompatible types in assign!",ctx.ID().getSymbol());
+        try {
+            int exprType = (int) visit(ctx.expression());
+            TablaSimbolos.Ident ident = this.symbolTable.buscar(ctx.ID().getText());
+            if (ident != null) {
+                if (!(ident instanceof TablaSimbolos.MethodIdent)) {
+                    if (((TablaSimbolos.VarIdent) ident).isConstant == false) {
+                        if (ident.type != exprType)
+                            reportError("Incompatible types in assign!", ctx.ID().getSymbol());
                         //todo: hacer un método polimórfico para reportError que incluya tipos incompatibles
-                }else
-                    reportError("Cannot assign to constant definition!!!",ctx.ID().getSymbol());
-            }else
-                reportError("Cannot assign to method definition!!!",ctx.ID().getSymbol());
-        }else
-            reportError("Undefined identifier!!!",ctx.ID().getSymbol());
+                    } else
+                        reportError("Cannot assign to constant definition!!!", ctx.ID().getSymbol());
+                } else
+                    reportError("Cannot assign to method definition!!!", ctx.ID().getSymbol());
+            } else
+                reportError("Undefined identifier!!!", ctx.ID().getSymbol());
+        }catch(TypeCheckerException e){
+            //podriamos considerar hacer otras verificaciones si fuera del caso
+        }
+
         return null;
     }
 
     @Override
     public Object visitMethodCallSingleCommand(AlphaCompilerParser.MethodCallSingleCommandContext ctx) {
+        //todo: verificar que exista y sea método y que la cantidad y los tipos de los argumentos coincidan con la declaración
         return super.visitMethodCallSingleCommand(ctx);
     }
 
     @Override
     public Object visitReturnSingleCommand(AlphaCompilerParser.ReturnSingleCommandContext ctx) {
+        // todo: verifica que el tipo de la expresión coincida con el tipo de retorno AL QUE PERTENEXCA ESTE RETURN ***
+        //TODO: no puede haber un return si no es en un método y toda función debe garantizar return completo
         return super.visitReturnSingleCommand(ctx);
     }
 
     @Override
     public Object visitIfSingleCommand(AlphaCompilerParser.IfSingleCommandContext ctx) {
+        //todo: el tipo de la expresión debe ser bool
         return super.visitIfSingleCommand(ctx);
     }
 
     @Override
     public Object visitWhileSingleCommand(AlphaCompilerParser.WhileSingleCommandContext ctx) {
+        //todo: el tipo de la expresión debe ser bool
         return super.visitWhileSingleCommand(ctx);
     }
 
@@ -101,6 +115,7 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
 
     @Override
     public Object visitArgumentList(AlphaCompilerParser.ArgumentListContext ctx) {
+        //todo: devolver lo necesario (Lista de números) para que funcione el methodCall
         return super.visitArgumentList(ctx);
     }
 
@@ -157,17 +172,52 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
 
     @Override
     public Object visitMethodSingleDeclaration(AlphaCompilerParser.MethodSingleDeclarationContext ctx) {
-        return super.visitMethodSingleDeclaration(ctx);
+        int tipo=-1;
+        Token type=null;
+        if(ctx.VOID()==null) {
+            type = (Token) visit(ctx.typeDenoter());
+            tipo = verifyType(type.getText());
+        }
+        else
+            tipo=4;
+
+        if (tipo!=-1){
+            LinkedList<Integer> params=null;
+            if(ctx.paramList()!=null)
+                params = (LinkedList<Integer>) visit(ctx.paramList());
+            else
+                params = new LinkedList<>();
+
+            TablaSimbolos.Ident ident = this.symbolTable.buscarNivelActual(ctx.ID().getText());
+            if(ident==null)
+                this.symbolTable.insertar(ctx.ID().getSymbol(),tipo,params,ctx);
+            else {
+                if (ident instanceof TablaSimbolos.VarIdent)
+                    this.symbolTable.insertar(ctx.ID().getSymbol(),tipo,params,ctx);
+                else
+                    reportError("Method name already defined!", ctx.ID().getSymbol());
+            }
+        }else
+            reportError("Undefined type!",type);
+        visit(ctx.singleCommand()); //chequear posibles errores en el cuerpo del método
+        return null;
     }
 
     @Override
     public Object visitParamList(AlphaCompilerParser.ParamListContext ctx) {
-        return super.visitParamList(ctx);
+        LinkedList<Integer> paramList = new LinkedList<>();
+        for(AlphaCompilerParser.ParamContext p : ctx.param())
+            paramList.add((int)visit(p));
+        return paramList;
     }
 
     @Override
     public Object visitParam(AlphaCompilerParser.ParamContext ctx) {
-        return super.visitParam(ctx);
+        Token t = (Token)visit(ctx.typeDenoter());
+        int type = verifyType(t.getText());
+        if (type == -1)
+            reportError("Undefined type!",t);
+        return type;
     }
 
     @Override
@@ -180,8 +230,89 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
         int returnType=-1;
         returnType = (int) visit(ctx.primaryExpression(0));
         for (int i = 1; i < ctx.primaryExpression().size(); i++) {
-            visit(ctx.operator(i-1));
-            visit(ctx.primaryExpression(i));
+            Token op = (Token) visit(ctx.operator(i-1));
+            int type2 = (int) visit(ctx.primaryExpression(i));
+            returnType=compatibleTypes(op,returnType,type2);
+            if (returnType==-1){
+                reportError("Incompatible types!!!",op);//todo: cambiar al otro reportError
+                throw new TypeCheckerException();
+            }
+        }
+        return returnType;
+    }
+
+    private int compatibleTypes(Token op, int t1, int t2){
+        int returnType = -1;
+        switch (op.getType()){
+            case(AlphaCompilerLexer.ADD):{
+                if((t1==0)&&(t2==0))
+                    returnType=0;
+                else if ((t1==3)&&(t2==3))
+                    returnType=3;
+                break;
+            }
+            case(AlphaCompilerLexer.SUB):{
+                if((t1==0)&&(t2==0))
+                    returnType=0;
+                break;
+            }
+            case(AlphaCompilerLexer.MUL):{
+                if((t1==0)&&(t2==0))
+                    returnType=0;
+                break;
+            }
+            case(AlphaCompilerLexer.DIV):{
+                if((t1==0)&&(t2==0))
+                    returnType=0;
+                break;
+            }
+            case(AlphaCompilerLexer.MOD):{
+                if((t1==0)&&(t2==0))
+                    returnType=0;
+                break;
+            }
+            case(AlphaCompilerLexer.EQEQ):{
+                if((t1==0)&&(t2==0))
+                    returnType=2;
+                else if ((t1==1)&&(t2==1))
+                    returnType=2;
+                else if ((t1==2)&&(t2==2))
+                    returnType=2;
+                else if ((t1==3)&&(t2==3))
+                    returnType=2;
+                break;
+            }
+            case(AlphaCompilerLexer.NOTEQ):{
+                if((t1==0)&&(t2==0))
+                    returnType=2;
+                else if ((t1==1)&&(t2==1))
+                    returnType=2;
+                else if ((t1==2)&&(t2==2))
+                    returnType=2;
+                else if ((t1==3)&&(t2==3))
+                    returnType=2;
+                break;
+            }
+            case(AlphaCompilerLexer.LESS):{
+                if((t1==0)&&(t2==0))
+                    returnType=2;
+                break;
+            }
+            case(AlphaCompilerLexer.MORET):{
+                if((t1==0)&&(t2==0))
+                    returnType=2;
+                break;
+            }
+            case(AlphaCompilerLexer.LESSEQ):{
+                if((t1==0)&&(t2==0))
+                    returnType=2;
+                break;
+            }
+            case(AlphaCompilerLexer.MOREEQ):{
+                if((t1==0)&&(t2==0))
+                    returnType=2;
+                break;
+            }
         }
         return returnType;
     }
@@ -193,16 +324,31 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
 
     @Override
     public Object visitIdPrimaryExpression(AlphaCompilerParser.IdPrimaryExpressionContext ctx) {
-        return super.visitIdPrimaryExpression(ctx);
+        //todo: verificar si el tipo de la expresión es entero en el caso de que SUB no sea null
+        int returnType=-1;
+        TablaSimbolos.Ident ident = this.symbolTable.buscar(ctx.ID().getText());
+        if (ident != null){
+            if (!(ident instanceof TablaSimbolos.MethodIdent)){
+                returnType= ident.type;
+            }else
+                reportError("Not a variable/constant identifier!!!",ctx.ID().getSymbol());
+        }else {
+            reportError("Undefined identifier!!!", ctx.ID().getSymbol());
+            throw new TypeCheckerException();
+        }
+
+        return returnType;
     }
 
     @Override
     public Object visitGroupPrimaryExpression(AlphaCompilerParser.GroupPrimaryExpressionContext ctx) {
-        return super.visitGroupPrimaryExpression(ctx);
+        ///todo: verificar si el tipo de la expresión es entero en el caso de que SUB no sea null
+        return visit(ctx.expression());
     }
 
     @Override
     public Object visitMethodCallPrimaryExpression(AlphaCompilerParser.MethodCallPrimaryExpressionContext ctx) {
+        //todo: lo mismo que el otro methodCall pero este retorna el tipo de la expresión
         return super.visitMethodCallPrimaryExpression(ctx);
     }
 
@@ -228,6 +374,29 @@ public class AlphaCompilerTypeChecker extends AlphaCompilerBaseVisitor<Object> {
 
     @Override
     public Object visitOperator(AlphaCompilerParser.OperatorContext ctx) {
-        return super.visitOperator(ctx);
+        if(ctx.ADD()!=null)
+            return ctx.ADD().getSymbol();
+        else if(ctx.SUB()!=null)
+            return ctx.SUB().getSymbol();
+        else if(ctx.MUL()!=null)
+            return ctx.MUL().getSymbol();
+        else if(ctx.DIV()!=null)
+            return ctx.DIV().getSymbol();
+        else if(ctx.MOD()!=null)
+            return ctx.MOD().getSymbol();
+        else if(ctx.EQEQ()!=null)
+            return ctx.EQEQ().getSymbol();
+        else if(ctx.NOTEQ()!=null)
+            return ctx.NOTEQ().getSymbol();
+        else if(ctx.LESS()!=null)
+            return ctx.LESS().getSymbol();
+        else if(ctx.MORET()!=null)
+            return ctx.MORET().getSymbol();
+        else if(ctx.LESSEQ()!=null)
+            return ctx.LESSEQ().getSymbol();
+        else if(ctx.MOREEQ()!=null)
+            return ctx.MOREEQ().getSymbol();
+        else
+            return null;
     }
 }
---
+++ src/typechecker/TypeCheckerException.java
@@ -0,0 +1,5 @@
+package typechecker;
+
+public class TypeCheckerException extends RuntimeException{
+    //todo: lo normal es que tenga constructor y que le pase mensaje específico
+}
